df01ad94f81d92e4120d8934d2303b26c1d55e61
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2022-06-18 17:46:06 -0400

    Register payments service and repository, and datetimebroker to the IoC container.

---
+++ OwnerSettlementsService.Api/Extensions/CustomDependenciesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OwnerSettlementsService.Core.Services;
+using OwnerSettlementsService.Core.Services.Abstractions;
+using OwnerSettlementsService.Data.DateTimes;
+using OwnerSettlementsService.Data.Repositories;
+using OwnerSettlementsService.Data.Repositories.Abstractions;
+
+namespace OwnerSettlementsService.Api.Extensions
+{
+    public static class CustomDependenciesExtensions
+    {
+        public static void RegisterCustomServices(this IServiceCollection services)
+        {
+            //Datetimes
+            services.AddScoped<IDateTimeBroker, SystemsDateTime>();
+            //Payments
+            services.AddScoped<IPaymentsService, PaymentsService>();
+            services.AddScoped<IPaymentsRepository, PaymentsRepository>();
+        }
+    }
+}
--- OwnerSettlementsService.Api/Startup.cs
+++ OwnerSettlementsService.Api/Startup.cs
@@ -35,6 +35,7 @@ namespace OwnerSettlementsService.Api
             });
 
             services.ConfigureDbContext(Configuration);
+            services.RegisterCustomServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
