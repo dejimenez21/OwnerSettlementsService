af87bee7f74b68cba265a301e266460f01bd5aec
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2022-06-18 17:24:20 -0400

    Add PostPayment endpoint

--- OwnerSettlementsService.Api/Controllers/PaymentsController.cs
+++ OwnerSettlementsService.Api/Controllers/PaymentsController.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OwnerSettlementsService.Core.Services.Abstractions;
 using OwnerSettlementsService.Data.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace OwnerSettlementsService.Api.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class PaymentsController : ControllerBase
     {
-        [HttpPost]
-        public async Task<IActionResult> PostPayment(Payment inputPayment)
+        private readonly IPaymentsService _paymentsService;
+
+        public PaymentsController(IPaymentsService paymentsService)
         {
-            return null;
+            _paymentsService = paymentsService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Payment>> PostPayment(Payment inputPayment)
+        {
+            var result = await _paymentsService.CreatePayment(inputPayment);
+            return Created($"/api/payments/{result.Data.Id}", result);
         }
     }
 }
