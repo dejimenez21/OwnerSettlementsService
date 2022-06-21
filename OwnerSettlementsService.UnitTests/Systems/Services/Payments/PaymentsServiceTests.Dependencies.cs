b1fc66cb52f89d4cd4934a56956f594c0f2ee0d1
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2022-06-20 22:21:54 -0400

    Add test to verify dependecy calls in RetrieveAllPayments method.

--- OwnerSettlementsService.UnitTests/Systems/Services/Payments/PaymentsServiceTests.Dependencies.cs
+++ OwnerSettlementsService.UnitTests/Systems/Services/Payments/PaymentsServiceTests.Dependencies.cs
@@ -12,7 +12,7 @@ namespace OwnerSettlementsService.UnitTests.Systems.Services.Payments
     public partial class PaymentsServiceTests
     {
         [Fact]
-        public async Task CreatePayment_Calls_InsertRepository()
+        public async Task CreatePayment_Calls_Repository_And_DateTimeBroker()
         {
             var today = new DateTime(2022, 5, 21);
             var inputPayment = new Payment
@@ -31,6 +31,14 @@ namespace OwnerSettlementsService.UnitTests.Systems.Services.Payments
             _paymentsRepositoryMock.Verify(broker => broker.Insert(It.Is(inputPayment, _comparer)), Times.Once);
             _paymentsRepositoryMock.Verify(broker => broker.SaveChangesAsync(), Times.Once);
             _dateTimeBrokerMock.Verify(broker => broker.GetCurrentDateTime(), Times.Once);
+        }
+
+        [Fact]
+        public async Task RetrieveAllPayments_Calls_Repository()
+        {
+            await _paymentsService.RetrieveAllPayments();
+
+            _paymentsRepositoryMock.Verify(broker => broker.SelectAll(), Times.Once);
         }
     }
 }
